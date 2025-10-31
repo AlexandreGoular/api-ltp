bec114d8f0dc14bb53ca1f1bd1ea47be9cd657f1
AlexandreGoular <alexandre.goulart@aluno.ifsp.edu.br>
2025-10-31 14:44:46 -0300

    render

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.database.db import init_db
 from app.routes.avaliacao_routes import avaliacao_bp
 from app.extensions import bcrypt, jwt
+import os
 
 app = Flask(__name__)
 init_db()
@@ -14,5 +15,5 @@ jwt.init_app(app)
 app.register_blueprint(avaliacao_bp, url_prefix="/avaliacoes")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port = os.environ.get('PORT', 3000))
 
