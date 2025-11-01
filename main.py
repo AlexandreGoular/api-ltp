9c50678e3f761766942242b907cb4fc405578c6f
Alexandre_Goulart <131091829+AlexandreGoular@users.noreply.github.com>
2025-11-01 17:39:58 -0300

    Restrict CORS to specific origin

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.config["JWT_SECRET_KEY"] = "super_secret_key"
 bcrypt.init_app(app)
 jwt.init_app(app)
 
-CORS(app) 
+CORS(app, origins=["http://localhost:5173"]) 
 
 app.register_blueprint(avaliacao_bp, url_prefix="/avaliacoes")
 app.register_blueprint(autor_bp, url_prefix="/autores")
