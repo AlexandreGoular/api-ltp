0ca6fc39da5fdff295bf7d6df582ddb13c8ea464
Alexandre_Goulart <131091829+AlexandreGoular@users.noreply.github.com>
2025-11-01 17:35:21 -0300

    cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.routes.avaliacao_routes import avaliacao_bp
 from app.routes.autor_routes import autor_bp
 from app.extensions import bcrypt, jwt
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 init_db()
@@ -12,6 +13,8 @@ app.config["JWT_SECRET_KEY"] = "super_secret_key"
 
 bcrypt.init_app(app)
 jwt.init_app(app)
+
+CORS(app) 
 
 app.register_blueprint(avaliacao_bp, url_prefix="/avaliacoes")
 app.register_blueprint(autor_bp, url_prefix="/autores")
